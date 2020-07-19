48a0e9b79716f57440be18ea1f9443d50270b9e2
Vibhakar Solanki <solankivibhakar82@gmail.com>
2020-07-19 20:32:06 +0530

    added doc string

--- RLMA/plugins/scrapers/ln/base/novel.py
+++ RLMA/plugins/scrapers/ln/base/novel.py
@@ -23,20 +23,21 @@ class Base():
         
         self.webpage = None
         self.prased_webpage = None
-
+        self.EMPTY = None
         self.link = link
         self.about = {
-            'CoverImage':'',
-            'name':'',
-            'chapters':'',
-            'author':'',
-            'status':'',
-            'release':'',
-            'updated':'',
-            'discription':''
+            'CoverImage':self.EMPTY,
+            'Name':self.EMPTY,
+            'Chapters':self.EMPTY,
+            'Author':self.EMPTY,
+            'Status':self.EMPTY,
+            'Release':self.EMPTY,
+            'Updated':self.EMPTY,
+            'Summary':self.EMPTY
 
         }
         self.chapter_list = {}
+        '''{chapter_number:{chapter_name:chapter_link}}'''
         
 
         self.headers = {'User-Agent': 'RLMA (https://github.com/gala0sup/RLMA) Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
@@ -58,9 +59,11 @@ class Base():
             self._get_webpage()
             self._prase_webpage()
             self._set_info()
-        except:
-            logging.error("unable to retrive info of (%s)",self.link)
 
+        except Exception as error:
+            logging.error(error)
+            logging.error("unable to retrive info of (%s)",self.link)
+            raise
 
     def info(self,full=False):
         if not self.initialized:
