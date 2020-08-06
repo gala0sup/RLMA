a4734285cd6576aade02254a58606e68cb469aad
gala <solankivibhakar82@gmail.com>
2020-08-06 19:14:44 +0530

    Set useragent for Downloader

--- RLMA/gui/gui.py
+++ RLMA/gui/gui.py
@@ -113,6 +113,11 @@ class RLMA(MDFloatLayout, MDApp):
         Config = RlmaConfig()
         Config.run()
         RlmaConfig.Config.write()
+        Config.Config.set(
+            "network",
+            "useragent",
+            "RLMA (https://github.com/gala0sup/RLMA) Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36",
+        )
         self.library = Library()
         self.set_RLMA()
 
