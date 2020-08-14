0d4c36a67957543ad263fcfcdb20c2a7eebfe62f
gala <solankivibhakar82@gmail.com>
2020-08-14 17:41:21 +0530

    Completed scraper for WordyBuzz CU-178m96[done]

--- RLMA/plugins/scrapers/ln/wordybuzz/item.py
+++ RLMA/plugins/scrapers/ln/wordybuzz/item.py
@@ -2,11 +2,9 @@ import logging
 from math import ceil
 from pathlib import Path
 import json
-import traceback
-
-from kivymd.uix.snackbar import Snackbar
 
 from plugins.scrapers.base.item import Base
+from bs4 import Tag
 
 logger = logging.getLogger("RLMA")
 
@@ -38,17 +36,18 @@ class Scraper(Base):
                         for k, v in JSON.items():
                             if k == "mainEntity":
                                 for key, value in v.items():
+
                                     if key == "author":
                                         if len(value):
                                             self.about["Author"] = value[0]["name"]
                                     elif key == "image":
-                                        self.about["CoverImage"] = value
+                                        self.about["CoverImage"] = value.strip()
                                     elif key == "name":
-                                        self.about["Name"] = value
+                                        self.about["Name"] = value.strip()
                                     elif key == "numberOfPages":
-                                        self.about["Chapters"] = int(value)
+                                        self.about["Chapters"] = int(value.strip())
                                     elif key == "description":
-                                        self.about["extra"] = value
+                                        self.about["extra"] = value.strip()
                         break
                     else:
                         pass
@@ -57,14 +56,38 @@ class Scraper(Base):
             div = self.prased_webpage.find("div", class_="summary text-muted").find_all(
                 "p"
             )
+            summary = ""
             for p in div:
                 if len(p.get_text()):
-                    self.about["Summary"] += p.get_text()
+                    summary += p.get_text()
+
+            self.about["Summary"] = summary
+
+            trs = self.prased_webpage.find(
+                "table", class_="table table-bordered"
+            ).find_all("tr")
+            for tr in trs:
+                if tr.find("th").get_text().lower() == "status":
+                    self.about["Status"] = tr.find("td").find("a").get_text()
+
             self.LibraryItemInstance.item_set_info()
         except Exception as error:
             logger.error(error)
             logger.debug("Scraper outdated")
-            Snackbar(
-                text="Scraper outdated please wait for Update..", duration=3
-            ).show()
-            print(traceback.format_exc())
+            raise
+
+        def _set_chapter_list(self):
+            try:
+                chapter_list_div = self.prased_webpage.find("div", class_="row pt-2")
+                for chapter_div in chapter_list_div:
+                    if isinstance(chapter_div, Tag):
+                        chapter_number = chapter_div.find("span").get_text()
+                        chapter_link = chapter_div.find("a")["href"]
+                        chapter_name = chapter_div.find("a").get_text()
+                        self.chapter_list[int(chapter_number)] = {
+                            chapter_name: chapter_link
+                        }
+            except:
+                logger.error(error)
+                logger.debug("Scraper outdated")
+                raise
