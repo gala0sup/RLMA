b64e7ce6d5486c6749dee6c89ba33e05055a0a73
gala <solankivibhakar82@gmail.com>
2020-08-22 17:23:26 +0530

    added path to fonts dir

--- RLMA/utils.py
+++ RLMA/utils.py
@@ -3,8 +3,11 @@ import pathlib
 from plyer import storagepath
 
 RLMAPATH = pathlib.Path(pathlib.Path(__file__).parent)
+
 APPLICATION_DIR = storagepath.get_application_dir()
 
 request_headers = {
     "useragent": "RLMA (https://github.com/gala0sup/RLMA) Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36"
 }
+
+font_path = RLMAPATH / "plugins" / "fonts"
