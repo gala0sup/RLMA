31e9d4d74dce4cff942353d680427053c7bdcdb1
gala <solankivibhakar82@gmail.com>
2020-09-04 15:26:42 +0530

    added Logger Class

--- RLMA/core/logger.py
+++ RLMA/core/logger.py
@@ -1,9 +1,33 @@
 import logging
-
 from rich.logging import RichHandler
 
-FORMAT = logging.Formatter("%(funcName)s : %(message)s")
-Logger = logging.getLogger("RLMA")
-Logger.setLevel(logging.INFO)
-Logger.addHandler(RichHandler())
-Logger.handlers[0].setFormatter(FORMAT)
+
+class Logger:
+    def __init__(self):
+        self.format = logging.Formatter("%(funcName)s : %(message)s")
+        self.level = logging.DEBUG
+
+        self.logger = logging.getLogger("RLMA")
+        self.logger.setLevel(self.level)
+        self.logger.addHandler(RichHandler())
+        self.logger.handlers[0].setFormatter(self.format)
+
+    def getLogger(self):
+        """returns logger instance"""
+        return self.logger
+
+    def info(self, message) -> None:
+        self.logger.info(message)
+
+    def debug(self, message) -> None:
+        self.logger.debug(message)
+
+    def warning(self, message) -> None:
+        self.logger.warning(message)
+
+    def critical(self, message) -> None:
+        self.logger.critical(message)
+
+    def setLevel(self, level: int):
+        self.logger.setLevel(level)
+
